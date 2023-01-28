6de5bf77e4c011eb9c5d56835c88734f3e6f9ef4
Munsa1 <munsamibenge65@gmail.com>
2023-01-28 13:36:41 +0200

    Add projects section html structure

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const menuBars = document.querySelector('.fa-bars');
 const overlay = document.querySelector('.overlay');
 const myLinks = document.querySelector('.my-links');
 const closeBar = document.querySelector('.fa-xmark');
+const link = document.querySelectorAll('.link');
 
 menuBars.addEventListener('click', () => {
   overlay.classList.remove('hidden');
@@ -12,3 +13,9 @@ closeBar.addEventListener('click', () => {
   overlay.classList.add('hidden');
   myLinks.classList.add('hidden');
 });
+
+
+// link.addEventListener('click', () => {
+//     overlay.classList.add('hidden');
+//     myLinks.classList.add('hidden');
+// });
