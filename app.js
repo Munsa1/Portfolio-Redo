f28f060f184e88583e23bcf822e7c8e388124f3c
Munsa1 <munsamibenge65@gmail.com>
2023-01-28 19:11:14 +0200

    Fix linter error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const menuBars = document.querySelector('.fa-bars');
 const overlay = document.querySelector('.overlay');
 const myLinks = document.querySelector('.my-links');
 const closeBar = document.querySelector('.fa-xmark');
-const link = document.querySelectorAll('.link');
+// const link = document.querySelectorAll('.link');
 
 menuBars.addEventListener('click', () => {
   overlay.classList.remove('hidden');
@@ -13,7 +13,6 @@ closeBar.addEventListener('click', () => {
   overlay.classList.add('hidden');
   myLinks.classList.add('hidden');
 });
-
 
 // link.addEventListener('click', () => {
 //     overlay.classList.add('hidden');
