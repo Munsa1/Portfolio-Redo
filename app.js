a897454cac0f3a672630c2355cbc1ffb992dcccd
Munsa1 <munsamibenge65@gmail.com>
2023-01-28 05:40:33 +0200

    Add javascript linter configuration

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const menuBars = document.querySelector('.fa-bars');
 const overlay = document.querySelector('.overlay');
 const myLinks = document.querySelector('.my-links');
-const closeBar = document.querySelector('.fa-xmark')
+const closeBar = document.querySelector('.fa-xmark');
 
+menuBars.addEventListener('click', () => {
+  overlay.classList.remove('hidden');
+  myLinks.classList.remove('hidden');
+});
 
-menuBars.addEventListener('click', ()=>{
-    overlay.classList.remove('hidden')
-    myLinks.classList.remove('hidden')
-})
-
-closeBar.addEventListener('click', ()=>{
-    overlay.classList.add('hidden')
-    myLinks.classList.add('hidden')
-})
+closeBar.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+  myLinks.classList.add('hidden');
+});
