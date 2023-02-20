7b3046e94a9132bbd5c3da791691e7299416cc53
Munsa1 <munsamibenge65@gmail.com>
2023-02-20 02:44:43 +0200

    Add remaining images for all cards

--- app.js
+++ app.js
@@ -18,3 +18,13 @@ closeBar.addEventListener('click', () => {
 //     overlay.classList.add('hidden');
 //     myLinks.classList.add('hidden');
 // });
+
+let contactInfo = {
+  Name: "Munsa Mibenge",
+  Email: "munsamibenge65@gmail.com",
+  Message: "Hi!, I would like to get in touch with you concerning some work"
+}
+
+console.log(contactInfo);
+
+localStorage.setItem("contact-info", JSON.stringify(contactInfo));
